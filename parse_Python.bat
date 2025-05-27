bc07a0b015c14cb4f19e3eca2c72f99da0820a60
Lopez, Luis R. <luis.lopez-3@leidos.com>
2025-05-27 11:25:01 -0500

    GPL compliance

--- parse_Python.bat
+++ parse_Python.bat
@@ -1,29 +1,46 @@
+echo off
 rem DRAG DROP TO PARSE AND MAKE BACKUP FOR CODE FILE 
 rem MUST BE VALID PYTHON TO WORK - NO SYNTAX ERRORS 
 rem TO VIEW AS FLOW YOU WILL NEED TO OBTAIN VFC2000.exe 
 rem EMAIL luis.r.lopez@outlook.com for access or check for a GitHub.com/RedhorseVR/VFCode open version
 rem NEED TO SET VFCTOOLPATH and COMMENTPARSERPATH to where you have the tools 
 
-@echo off
-set BAT_FILE_PATH=%~dp0
+
+set BAT_FILE_PATH=%CD%
 echo The absolute path of this batch file is: %BAT_FILE_PATH%
 
-SET VFCTOOLPATH=C:\VFC1.0
-SET COMMENTPARSERPATH=%BAT_FILE_PATH%
-
-
-python %COMMENTPARSERPATH%\pyCommentParser.py %1
 
 echo off
 set filepath=%1
 for %%A in ("%filepath%") do set filename=%%~nxA
 echo Filename: %filename%
+ECHO %CD%/%filename%
+echo -----------------------
 
-copy %filename% _%filename%
+echo off
+echo Processing files...
+
+for %%F in (%*) do (
+	echo Processing: "%%F"
+	echo root: %filename%
+	echo Folder: %~dp1
+
+	copy %1 %~dp1\_%filename%
+
+	python %BAT_FILE_PATH%\parse_Python.py %1
+
+
+)
+
+
+
+
+
 
 dir
 
+echo ---------------- %1.vfc
 
-start %VFCTOOLPATH%\VFC2000.exe %1.vfc
+start VFC2000 %1.vfc
 
-pause
+
